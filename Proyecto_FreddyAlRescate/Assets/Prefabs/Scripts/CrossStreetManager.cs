eb4ea45374c8f98390f085dd94ff076610ae6b58
TaiRFest156 <144045527+TaiRFest156@users.noreply.github.com>
2025-04-26 19:24:16 -0300

    Mejorar escenas del minijuego "cruzar la calle"
    Luego de tocar los botones, ahora las escenas tienen una secuencia de viñetas para mostrar lo que sucede.

--- Proyecto_FreddyAlRescate/Assets/Prefabs/Scripts/CrossStreetManager.cs
+++ Proyecto_FreddyAlRescate/Assets/Prefabs/Scripts/CrossStreetManager.cs
@@ -3,21 +3,18 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class LookBothWaysManager : MonoBehaviour
+public class CrossStreetManager : MonoBehaviour
 {
     [SerializeField] private GameObject panel1;
     [SerializeField] private GameObject panel2;
-    
 
     [SerializeField] private float delay = 2f;
-
 
     void Start()
     {
         panel1.SetActive(false);
         panel2.SetActive(true);
         StartCoroutine(SwitchPanels());
-        StartCoroutine(WaitAndReturn());
     }
 
     IEnumerator SwitchPanels()
@@ -26,15 +23,5 @@ public class LookBothWaysManager : MonoBehaviour
 
         panel1.SetActive(true);
         panel2.SetActive(false);
-    }
-
-    IEnumerator WaitAndReturn()
-    {
-        yield return new WaitForSeconds(5f); // tiempo de lo que va a durar la animacion de viñetas
-
-        // se guarda el estado directamente
-        PlayerPrefs.SetInt("LookedBothWays", 1);
-
-        SceneManager.LoadScene("MainStreet");
     }
 }
--- Proyecto_FreddyAlRescate/Assets/Prefabs/Scripts/TrafficLightManager.cs
+++ Proyecto_FreddyAlRescate/Assets/Prefabs/Scripts/TrafficLightManager.cs
@@ -5,15 +5,34 @@ using System.Collections;
 
 public class TrafficLightScene : MonoBehaviour
 {
+    // paneles que tapan sus respectivas viñetas del fondo
+    [SerializeField] private GameObject panel2;
+    [SerializeField] private GameObject panel3;
+
+    // tiempo de espera para mostrar cada viñeta
+    [SerializeField] private float delay1 = 2f;
+    [SerializeField] private float delay2 = 2f; 
+
     void Start()
     {
-        // se muestra mensaje de importancia y animacion
+        
         StartCoroutine(WaitAndReturn());
+        StartCoroutine(RevealPanels());
     }
+
+    IEnumerator RevealPanels()
+    {
+        yield return new WaitForSeconds(delay1);
+        panel2.SetActive(false); // muestra viñeta 2
+
+        yield return new WaitForSeconds(delay2);
+        panel3.SetActive(false); // muestra viñeta 3
+    }
+
 
     IEnumerator WaitAndReturn()
     {
-        yield return new WaitForSeconds(3f); // tiempo de la animación
+        yield return new WaitForSeconds(6f); // tiempo de la animación
 
         // guardar el estado directamente
         PlayerPrefs.SetInt("LookedAtTrafficLight", 1);
