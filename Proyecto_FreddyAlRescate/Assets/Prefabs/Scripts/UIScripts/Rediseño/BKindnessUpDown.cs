124b276c5424c6bf3e2e0517f603fc5b21e19500
Johanna_VJ <165029467+Chaves-Johanna-Milagro@users.noreply.github.com>
2025-05-13 11:46:11 -0300

    Barra de amabilidad
    Ya funciona la barra de amabilidad en cuanto a subir/bajar. se uso los clicks del mouse como prueba

---
+++ Proyecto_FreddyAlRescate/Assets/Prefabs/Scripts/UIScripts/Rediseño/BKindnessUpDown.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class BKindnessUpDown : MonoBehaviour
+{
+    private RectTransform _nowBar;
+
+    private float _maxY = -270f; //limite superior
+    private float _minY = -720f; //limite inferior
+
+    private float _amount = 5f; // la cantidad de frames que se movera
+
+    void Start()
+    {
+        _nowBar = transform.Find("Now").GetComponent<RectTransform>();
+
+
+        // Cargar la posición guardada
+        float savedY = Mathf.Clamp(KindnessStatus.GetNowBarY(), _minY, _maxY);
+        Vector2 newPos = _nowBar.anchoredPosition;
+        newPos.y = savedY;
+        _nowBar.anchoredPosition = newPos;
+    }
+
+    public void Update()//prueba de funcionamiento
+    {
+        if (Input.GetMouseButtonDown(0)) GoodDecision();
+        if (Input.GetMouseButtonDown(1)) BadDecision();
+    }
+
+    public void GoodDecision()
+    {
+        Vector2 newPos = _nowBar.anchoredPosition + new Vector2(0f,_amount );
+        newPos.y = Mathf.Clamp(newPos.y, _minY, _maxY);
+        _nowBar.anchoredPosition = newPos;
+
+        KindnessStatus.SetNowBarY(newPos.y); // Guardar la nueva posición
+    }
+
+    public void BadDecision()
+    {
+        Vector2 newPos = _nowBar.anchoredPosition + new Vector2(0f, -_amount);
+        newPos.y = Mathf.Clamp(newPos.y, _minY, _maxY);
+        _nowBar.anchoredPosition = newPos;
+
+        KindnessStatus.SetNowBarY(newPos.y); // Guardar la nueva posición
+    }
+}
