9083d045b55de062bcb09a010ef96ae7af3400bc
Johanna_VJ <165029467+Chaves-Johanna-Milagro@users.noreply.github.com>
2025-06-14 21:18:19 -0300

    Mas animacion del player
    Ahora tambien cambia la animacion si esque se cambio de ropa

--- Proyecto_FreddyAlRescate/Assets/Scripts/Rediseño/Player/CharacterAnim.cs
+++ Proyecto_FreddyAlRescate/Assets/Scripts/Rediseño/Player/CharacterAnim.cs
@@ -5,15 +5,14 @@ public class CharacterAnim : MonoBehaviour
     private CharacterClickMove _moveChar;
 
     private Animator _anim;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         _moveChar = GetComponent<CharacterClickMove>();
-
         _anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -21,26 +20,58 @@ public class CharacterAnim : MonoBehaviour
             Vector3 clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             clickPos.z = -0.1f;
 
-            if (clickPos.x > transform.position.x)
-            {
-                _anim.SetBool("R_Walk_PJ", true);
-                _anim.SetBool("Idle_PJ", false);
-                _anim.SetBool("L_Walk_PJ", false);
-            }
-            else if (clickPos.x < transform.position.x)
-            {
-                _anim.SetBool("L_Walk_PJ", true);
-                _anim.SetBool("Idle_PJ", false);
-                _anim.SetBool("R_Walk_PJ", false);
-            }
+            HandleWalkAnimation(clickPos);
         }
 
-        // Siempre chequeamos el estado de movimiento, cada frame
-        if (_moveChar.IsMoving() == false)
+        HandleIdleTransition();
+    }
+
+    void HandleWalkAnimation(Vector3 clickPos)
+    {
+        ResetAllBools();
+
+        bool useRP = ChecksStatus.IsCheckActive("Morning2.0", 1); // verificamos si se cambio de ropa usando el check activo/inactivo
+
+        if (clickPos.x > transform.position.x)
         {
-            _anim.SetBool("Idle_PJ", true);
-            _anim.SetBool("R_Walk_PJ", false);
-            _anim.SetBool("L_Walk_PJ", false);
+            if (useRP)
+                _anim.SetBool("R_Walk_RP", true);
+            else
+                _anim.SetBool("R_Walk_PJ", true);
         }
+        else if (clickPos.x < transform.position.x)
+        {
+            if (useRP)
+                _anim.SetBool("L_Walk_RP", true);
+            else
+                _anim.SetBool("L_Walk_PJ", true);
+        }
+    }
+
+    void HandleIdleTransition()
+    {
+        if (!_moveChar.IsMoving())
+        {
+            ResetAllBools();
+
+            bool useRP = ChecksStatus.IsCheckActive("Morning2.0", 1); // Revisamos de nuevo porsi la dudas
+
+            if (useRP)
+                _anim.SetBool("Idle_RP", true);
+            else
+                _anim.SetBool("Idle_PJ", true);
+        }
+    }
+
+
+    void ResetAllBools()
+    {
+        _anim.SetBool("R_Walk_PJ", false);
+        _anim.SetBool("L_Walk_PJ", false);
+        _anim.SetBool("Idle_PJ", false);
+
+        _anim.SetBool("R_Walk_RP", false);
+        _anim.SetBool("L_Walk_RP", false);
+        _anim.SetBool("Idle_RP", false);
     }
 }
