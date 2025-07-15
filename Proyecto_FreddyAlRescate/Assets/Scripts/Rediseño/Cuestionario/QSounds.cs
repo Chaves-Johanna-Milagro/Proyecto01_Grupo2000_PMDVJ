f53fab78bb55ba7d46147f30c56bf8035dc4af43
Johanna_VJ <165029467+Chaves-Johanna-Milagro@users.noreply.github.com>
2025-07-14 21:48:39 -0300

    Cuestionario pt3
    Ya tiene las diez preguntas con sus respectivos audios, solo se muestran cinco preguntas aleatorias

---
+++ Proyecto_FreddyAlRescate/Assets/Scripts/Rediseño/Cuestionario/QSounds.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QSounds : MonoBehaviour
+{
+    private AudioSource _qSound;
+
+    private void Start()
+    {
+        //Obtiene su audio sourse
+        _qSound = GetComponent<AudioSource>();
+
+        if (_qSound != null) _qSound.Play();
+    }
+
+
+}
