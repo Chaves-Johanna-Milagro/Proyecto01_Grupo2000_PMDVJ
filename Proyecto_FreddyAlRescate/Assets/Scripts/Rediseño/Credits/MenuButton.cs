9538926dba70b681a8cc0e2de632e9cd25513a47
Johanna_VJ <165029467+Chaves-Johanna-Milagro@users.noreply.github.com>
2025-07-18 00:48:20 -0300

    Game Over pt1
    Se creo la escena y se pusieron los botones de salir y volver al menu

---
+++ Proyecto_FreddyAlRescate/Assets/Scripts/Rediseño/Credits/MenuButton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour //script del boton de menu de los creditos
+{
+    private Button _bMenu;
+    private void Start()
+    {
+        _bMenu = GetComponent<Button>();
+
+        _bMenu.onClick.AddListener(ReturnMenu);
+    }
+
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene("Menu2.0");
+
+        PlayerNameStatus.ResetPlayerName(); //resetiar todo
+        LevelGameStatus.ClearLevel();
+        ChecksStatus.ResetAllChecks();
+        KindnessStatus.ResetKindness();
+        CinematicStatus.ResetCinematicStatus();
+        DecisionStatus.ResetDecisionStatus();
+        RecessStatus.ResetRecessStatus();
+        PauseStatus.ResetPause();
+
+        Debug.Log("volviendo al menu");
+    }
+}
---
+++ Proyecto_FreddyAlRescate/Assets/Scripts/Rediseño/GameOver/GameOver.cs
@@ -0,0 +1,41 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    private Button _bMenu;
+    private Button _bExit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _bMenu = transform.Find("BMenu").GetComponent<Button>();
+        _bExit = transform.Find("BExit").GetComponent<Button>();
+
+        _bMenu.onClick.AddListener(ReturnMenu);
+        _bExit.onClick.AddListener(ExitGame);
+    }
+
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene("Menu2.0");
+
+        PlayerNameStatus.ResetPlayerName(); //resetiar todo
+        LevelGameStatus.ClearLevel();
+        ChecksStatus.ResetAllChecks();
+        KindnessStatus.ResetKindness();
+        CinematicStatus.ResetCinematicStatus();
+        DecisionStatus.ResetDecisionStatus();
+        RecessStatus.ResetRecessStatus();
+        PauseStatus.ResetPause();
+
+        Debug.Log("volviendo al menu");
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit(); //cierra el ejecutable
+    }
+}
